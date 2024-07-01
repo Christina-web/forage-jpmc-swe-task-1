9c0e623070a847064748e0713f875f52cd22ad8c
Christina Manjappallil <christinamanjappallil@gmail.com>
2024-07-01 11:19:12 -0400

    Implemented stock data processing and unit tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,12 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      self.assertEqual(stock, quote['stock'])
+      self.assertEqual(bid_price, quote['top_bid']['price'])
+      self.assertEqual(ask_price, quote['top_ask']['price'])
+      self.assertEqual(price, (quote['top_bid']['price'] + quote['top_ask']['price']) / 2)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +21,19 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      self.assertEqual(stock, quote['stock'])
+      self.assertEqual(bid_price, quote['top_bid']['price'])
+      self.assertEqual(ask_price, quote['top_ask']['price'])
+      self.assertEqual(price, (quote['top_bid']['price'] + quote['top_ask']['price']) / 2)
 
 
   """ ------------ Add more unit tests ------------ """
-
+def test_getRatio(self):
+  self.assertEqual(getRatio(100, 200), 0.5)
+  self.assertEqual(getRatio(100, 100), 1.0)
+  self.assertIsNone(getRatio(100, 0))
 
 
 if __name__ == '__main__':
